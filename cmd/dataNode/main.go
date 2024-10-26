28944b47e62fe418c3608be5e1b8e3dcc4ff1d88
Max Nikitenko <max.nikitenko@namecheap.com>
2024-10-26 15:49:52 +0300

    feat(cli): update DataNode with new storage;

--- cmd/dataNode/main.go
+++ cmd/dataNode/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"LogDb/internal/adapters/filters"
-	"LogDb/internal/adapters/log_transformers"
-	"LogDb/internal/adapters/presenters"
-	"LogDb/internal/adapters/repository"
+	"LogDb/internal/adapters/data/loader"
+	"LogDb/internal/adapters/datastor"
+	"LogDb/internal/adapters/index"
 	"LogDb/internal/adapters/serializer"
-	"bufio"
+	"LogDb/internal/domain"
 	"fmt"
-	"log"
-	"os"
 	"runtime"
 	"time"
 )
-
-const ApacheLogsFile = "assets/logfiles.log"
 
 func MemoryUsage() {
 	var m runtime.MemStats
@@ -38,61 +33,15 @@ func init() {
 	}()
 }
 
+const BaseDir = ".storage"
+const ApacheLogsFile = "assets/logfiles.log"
+
 func main() {
-
-	codec := &serializer.BinarySerializer{}
-	writer := repository.NewDataFileWriter(codec, time.Now())
-	err := writer.Open()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer writer.Close()
-	// Write some data
-	loadApacheLogsFromFile(ApacheLogsFile, writer, 10_000)
-
-	reader := repository.NewDataFileReader(codec)
-	err = reader.Open("2024-10-25.514848382-work.chunk")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer reader.Close()
-	filterSet := filters.NewDateRangeFilter(time.Now().Add(-time.Hour), time.Now().Add(+time.Hour))
-	// Read some data
-	result, err := reader.Query(filterSet)
-	if err != nil {
-		log.Fatalf("Error querying storage: %v", err)
-	}
-	presenter := presenters.NewStringPresenter()
-	for _, record := range result.Records {
-		fmt.Println(presenter.Present(record))
-	}
-	fmt.Printf("report: %s scanned %d records %d hits in %v seconds\n", result.Report.Id, result.Report.Count, result.Report.Hits, result.Report.ElapsedTime.Seconds())
-
-}
-
-func loadApacheLogsFromFile(fileName string, storage *repository.DataFileWriter, limit int) {
-
-	fh, error := os.Open(fileName)
-	if error != nil {
-		log.Fatalf("Error opening file: %v", error)
-	}
-	defer fh.Close()
-
-	transformer := log_transformers.NewApacheLogTransformer()
-	scanner := bufio.NewScanner(fh)
-	var count int
-	for scanner.Scan() {
-		if limit > 0 && count >= limit {
-			break
-		}
-		line := scanner.Bytes()
-		record := transformer.FromBytes(line)
-		err := storage.AppendLogRecord(record)
-		if err != nil {
-			log.Fatalf("Error appending record: %v", err)
-		}
-		count++
-	}
-	fmt.Printf("Loaded %d logs from file\n", count)
-	os.Exit(0)
+	codec := serializer.Default
+	idx := index.NewTimestamp(BaseDir, codec)
+	stor := datastor.NewPersistentStorage(BaseDir, codec, idx)
+	defer stor.Close()
+	loader.LoadApacheLogsFromFile(ApacheLogsFile, func(record *domain.LogRecord) error {
+		return stor.StoreLogRecord(record)
+	}, 0)
 }
